e60fbe09901f88392482deb0cc39e5079f2103dd
Mengying Xiao <113824239+ChloeXiao0409@users.noreply.github.com>
2024-02-05 05:23:30 +0000

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Chloe Xiao",
+  title: "Web Designer & Full-stack Developer",
+  email: "chloexiao0409@gmail.com",
+  gitHub: "https://github.com/ChloeXiao0409",
+  instagram: "https://www.instagram.com/chloe_kiki0827/",
+  linkedIn: "https://www.linkedin.com/in/mengying-xiao-4a035a250/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/ChloeXiao15",
+  youTube: "https://www.youtube.com/channel/UCNI8i130ORhtkWHyG6D43hg",
 };
 
 const primaryColor = "#4E567E";
